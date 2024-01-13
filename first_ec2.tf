544a5bff338b65ac191d6d2b5bfdf2284a11ee52
terraform-learner <83661450+terraform-learner@users.noreply.github.com>
2024-01-13 11:05:16 +0800

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
   
 }
 resource "aws_instance" "myec2" {
-     ami = "ami-02f26adf094f51167"
+     ami = "ami-0005e0cfe09cc9050"
      instance_type = "t2.micro"
      tags = {
       Name = "web"
